7272bca19690f10384733961360f02d41fcf1db7
Blake Neely <blakeneely21@gmail.com>
2019-09-02 23:10:10 -0500

    loaded firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC6A42XuayUaOcx2nvHUOLbbXb17pfhLjQ",
+    authDomain: "train-scheduler-86274.firebaseapp.com",
+    databaseURL: "https://train-scheduler-86274.firebaseio.com",
+    projectId: "train-scheduler-86274",
+    storageBucket: "",
+    messagingSenderId: "466444663514",
+    appId: "1:466444663514:web:50c576aaf29f5e89"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
